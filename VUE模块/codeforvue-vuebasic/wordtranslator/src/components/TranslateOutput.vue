73f3e90673f3525a1a3da4450597106a127449f8
Administrator <280828574@qq.com>
2018-04-26 18:12:49 +0800

    21231

---
+++ VUE模块/codeforvue-vuebasic/wordtranslator/src/components/TranslateOutput.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="text-success">{{translatedText}}</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'translateOutput',
+  props:[
+    "translatedText"
+  ]
+}
+</script>
+
+<style>
+
+</style>
