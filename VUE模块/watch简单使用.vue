4fd3e64c9049054a3decfdc5b81f298c449efcf8
Administrator <280828574@qq.com>
2018-04-27 09:52:58 +0800

    21231

---
+++ VUE模块/watch简单使用.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="watch">
+    <input type="text" v-model:value="childrens.name" />
+    <input type="text" v-model:value="lastName" />
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name:'hello',
+    data(){
+          return{
+            childrens: {
+              name: '小小',
+              age: 20,
+              sex: '男'
+            },
+            tdArray:["1","2"],
+            lastName:"张三"
+          }
+        },
+    watch:{
+      childrens:{
+        handler:function(val,oldval){
+          console.log(oldval.name)
+          console.log(val.name)
+        },
+        deep:true//对象内部的属性监听，也叫深度监听
+      },
+      // 'childrens.name':function(val,oldval){
+      //   console.log(val)
+      // },//键路径必须加上引号
+
+      lastName:function(val,oldval){
+        console.log(this.lastName)
+      }
+    },//以V-model绑定数据时使用的数据变化监测
+  };
+</script>
+
+<style scoped>
+
+</style>
